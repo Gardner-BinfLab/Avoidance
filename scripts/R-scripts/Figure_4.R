f14f2435a4b0fa7f33f714d15da27536c35bd587
Sinan U. Umu <sinanugur@gmai.com>
2016-03-21 22:59:16 +0100

    R

--- scripts/R-scripts/Figure_4.R
+++ scripts/R-scripts/Figure_4.R
@@ -2,7 +2,7 @@ CDS.core.exp=read.csv("/home/suu13/projects/antisense/core_gene_expression/combi
 Rfam.core.exp=read.csv("/home/suu13/projects/antisense/core_gene_expression/combined.rfam.expressions.txt",header = F)
 
 
-pdf("Figure_3.RData",height = 9,width = 9)
+pdf("Figure_4.pdf",height = 9,width = 9)
 par(oma=c(1,4,1,2),mar=c(5,3,2,1),xpd=NA) #c(bottom, left, top, right)
 boxplot(log10(Rfam.core.exp$V1[Rfam.core.exp$V1 !=0]),log10(CDS.core.exp$V1[CDS.core.exp$V1 !=0]),
         names=c("Core ncRNA genes","Core protein coding genes"),col=c("firebrick","cornflowerblue"))
@@ -13,4 +13,4 @@ mtext("Gene type",side=1,outer = F,line=3,cex=1.8)
 
 dev.off()
 
-save.image(file="Figure_3.RData")
+save.image(file="Figure_4.RData")
